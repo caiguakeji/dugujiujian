68cf30ab1ae9c0df98e97cbf058a47100acfe2ef
caigua <caiguakeji@163.com>
2021-01-20 22:04:02 +0800

    Initial commit

---
+++ src/main/java/com/caigua/java/TestJava.java
@@ -0,0 +1,11 @@
+package com.caigua.java;
+
+/**
+ * @author jerry
+ * @create 2021-01-20-16:45
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("helo,world");
+    }
+}
